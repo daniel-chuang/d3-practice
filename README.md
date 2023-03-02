5f53147bbd60929e2cbb9a1b8ee5b393b7fc95d7
daniel-chuang <sharpincludecode@gmail.com>
2023-03-02 01:53:52 -0700

    resized the chart

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # d3-practice
 
-![Bar Chart Image]("./images/bar-chart.png")
+![Bar Chart Image]('https://github.com/daniel-chuang/d3-practice/blob/main/images/bar-chart.png')
 [Bar Chart]("./bar-chart/index.html")
--- bar-chart/graphic.js
+++ bar-chart/graphic.js
@@ -3,35 +3,32 @@ import * as d3 from 'https://unpkg.com/d3?module'
 fetch("./data.json")
   .then((response) => response.json())
   .then((data) => {
-    console.log(data.data);
 
     // Setting up the years
     var yearsDate = data.data.map(function (item) {
       return new Date(item[0]);
     });
-    console.log(yearsDate)
 
 
-    const w = 1120;
-    const h = 750;
+    const w = 896;
+    const h = 600;
 
     const space = w / (data.data.length + 1);
 
     const yScale = d3.scaleLinear()
       .domain([0, d3.max(data.data, (d) => d[1])])
-      .range([730, 0]);
+      .range([h - 20, 0]);
 
     
-    console.log(data.data); 
     var xScale = d3.scaleTime()
     .domain(d3.extent(yearsDate))
     .range([0, w]);
 
     const svg = d3.select("div")
       .append("svg")
-      .attr("width", w + 50)
+      .attr("width", w + 60)
       .attr("height", h + 100)
-      .attr("id", "title")
+      .attr("id", "title");
 
     // Making tooltip
     let tooltip = d3.select("div")
@@ -39,7 +36,6 @@ fetch("./data.json")
       .attr("class", "tooltip");
 
     function moveTooltip(event) {
-      console.log("moving tooltip");
       const [x, y] = d3.pointer(event);
       tooltip.style("top", `${h}px`);
       tooltip.style("left", `${x + 10 + ((window.innerWidth - w) / 2)}px`);
