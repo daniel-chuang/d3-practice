a08991349c7b5a3cf481d108075674938c302622
daniel-chuang <sharpincludecode@gmail.com>
2023-03-02 01:46:49 -0700

    working bar chart

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # d3-practice
 
+![Bar Chart Image]("./images/bar-chart.png")
 [Bar Chart]("./bar-chart/index.html")
--- bar-chart/graphic.js
+++ bar-chart/graphic.js
@@ -5,23 +5,36 @@ fetch("./data.json")
   .then((data) => {
     console.log(data.data);
 
+    // Setting up the years
+    var yearsDate = data.data.map(function (item) {
+      return new Date(item[0]);
+    });
+    console.log(yearsDate)
+
+
     const w = 1120;
-    const h = 730;
+    const h = 750;
 
     const space = w / (data.data.length + 1);
 
     const yScale = d3.scaleLinear()
       .domain([0, d3.max(data.data, (d) => d[1])])
-      .range([h, 0]);
+      .range([730, 0]);
 
-    const svg = d3.select("body")
+    
+    console.log(data.data); 
+    var xScale = d3.scaleTime()
+    .domain(d3.extent(yearsDate))
+    .range([0, w]);
+
+    const svg = d3.select("div")
       .append("svg")
       .attr("width", w + 50)
       .attr("height", h + 100)
       .attr("id", "title")
 
     // Making tooltip
-    let tooltip = d3.select("body")
+    let tooltip = d3.select("div")
       .append("span")
       .attr("class", "tooltip");
 
@@ -29,7 +42,7 @@ fetch("./data.json")
       console.log("moving tooltip");
       const [x, y] = d3.pointer(event);
       tooltip.style("top", `${h}px`);
-      tooltip.style("left", `${x + 10}px`);
+      tooltip.style("left", `${x + 10 + ((window.innerWidth - w) / 2)}px`);
     }
 
     svg.selectAll("rect")
@@ -41,27 +54,34 @@ fetch("./data.json")
       .attr("data-gdp", (d) => d[1])
       .attr("fill", "red")
       .attr("x", (d, i) => i * space + 50)
-      .attr("y", (d, i) => yScale(d[1]))
+      .attr("y", (d, i) => yScale(d[1]) + 20)
       .attr("width", space - 1)
-      .attr("height", (d) => h - yScale(d[1]))
+      .attr("height", (d) => h - 20 - yScale(d[1]))
       .on("mouseover", (event, d) => {
         moveTooltip(event);
         tooltip.html("<p>" + d[1] + " Billion</p>");
-        tooltip.text(data.data[i] + "Billion");
       })
       ;
 
       // Adding axis
       const yAxis = d3.axisLeft(yScale);
+      const xAxis = d3.axisBottom(xScale)
+        .tickFormat(d3.timeFormat('%Y'));
 
+      // For y axis
       svg.append("g")
-        .attr("transform", "translate(" + 50 + "," + 0 + ")")
+        .attr("transform", "translate(" + 50 + "," + 20 + ")")
         .call(yAxis);
+
+      // For x axis
+      svg.append("g")
+        .attr("transform", "translate(" + 50 + "," + h + ")")
+        .call(xAxis);
 
       // Adding axis label
       svg.append("text")
         .text("Gross Domestic Product, in billions of dollars")
         .attr("transform", "rotate(-90)")
-        .attr("x", -300)
+        .attr("x", -330)
         .attr("y", 80)
   });
